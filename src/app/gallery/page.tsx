de53b87176cdaa89fdff3ffa2f0d25f25a6e9cac
Armaan Gupta <ohayouarmaan@gmail.com>
2025-01-27 00:16:13 +0530

    FUCK ESLINT

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,8 +2,8 @@ import Slider from "@/components/carousel"
 
 export default function() {
   return <div className="flex flex-col items-center justify-center h-[100vh]">
-    <h1 className="text-4xl text-white font-bold uppercase absolute top-[10vh]">CUTE MEMORIES :3</h1>
-    <div className="w-[80vw] h-[40vh]">
+    <h1 className="text-2xl text-white h-[10vh] absolute text-vertical left-[20px] top-1/2 translate-y-[-50%] z-[10] font-bold uppercase absolute top-[10vh]">CUTE MEMORIES :3</h1>
+    <div className="w-[80vw]">
       <Slider />
     </div>
   </div>
--- src/app/reason1/page.tsx
+++ src/app/reason1/page.tsx
@@ -6,15 +6,15 @@ export default function() {
   <Image 
     src={"/samaan-on-car.png"}
     alt={"samaan"}
-    width={400}
-    height={400}
+    width={300}
+    height={300}
     className="pixel-border absolute top-1/2 translate-y-[-80%] animate-fade-in-longer left-[40px] p-5"
   />
   <Image 
     src={"/us-staring.jpg"}
     alt={"samaan"}
-    width={400}
-    height={400}
+    width={300}
+    height={300}
     className="pixel-border absolute top-1/2 translate-y-[-20%] animate-fade-in right-[40px] p-5"
   />
   <div className="flex items-center justify-center h-[100vh] w-[100%]">
@@ -22,7 +22,7 @@ export default function() {
       <h1 className="text-4xl font-bold w-full text-center text-white">
         HAPPY BIRTHDAY BB!!
       </h1>
-      <p className="text-white font-bold text-lg p-4">
+      <p className="text-white font-bold text-md p-4">
         SAM I LOVE YOU SOOOOOOO FUCKING MUCH I SWEAR TO GOD YOU'RE SO CUTEEEEEEEEEEEEEEAAAAAAAAA
         Okay, I know I fuck up a lot sometimes. But trust me I actually do love you a lot. You're the best
         thing which has happened to me in a while, not just in a while BUT ACTUALLY IN MY ENTIRE FUCKING LIFE
